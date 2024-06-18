18a51e3ce8be74db0afe75d3cd5684f9e4e8f97b
Matheus do Vale <matheusalmeida4100@gmail.com>
2024-06-18 15:19:36 -0300

    Create load_modules.sh

---
+++ utils/load_modules.sh
@@ -0,0 +1,8 @@
+module load haddock-tools/1.0
+module load protein-docking/haddock/2.4
+module load python/3.9.5/miniconda_4.10.3
+module load Haddock/2.4
+module load chimera/1.15
+module load compillers/gcc/10.2.0
+
+module load freesasa/2.0.3
